84db4c84396a3c0100af95adc527572585b5477f
Dat Doan <doantandat.fs@gmail.com>
2023-04-17 17:43:45 +0700

    refactor: refactor challenge helper

--- contracts/ChallengeFactory.sol
+++ contracts/ChallengeFactory.sol
@@ -3,15 +3,14 @@ pragma solidity ^0.8.19;
 
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import {EnumerableMapUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/structs/EnumerableMapUpgradeable.sol";
-import {SafeMathUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/math/SafeMathUpgradeable.sol";
 
 contract ChallengeFactory is OwnableUpgradeable {
-    using SafeMathUpgradeable for uint256;
     using EnumerableMapUpgradeable for EnumerableMapUpgradeable.AddressToUintMap;
 
     struct Challenge {
         string title;
         string rules;
+        bool isDisbursed;
         uint16 numTakers;
         uint256 prizePool;
         uint256 startTime;
@@ -23,39 +22,50 @@ contract ChallengeFactory is OwnableUpgradeable {
 
     uint256 public constant MIN_PRIZE_POOL = 0.01 ether;
     uint256 public constant MIN_DURATION = 1 days;
+    uint256 public constant MAX_DURATION = 15 days;
+    uint256 private _challengeIdCounter = 0;
+
     Challenge[] public challenges;
 
-    event ChallengeCreated(uint id, string title, string rules, uint256 prizePool);
+    event ChallengeCreated(uint indexed id, string title, string rules, uint256 prizePool);
 
     modifier onlyChallengeNotEnded(uint256 _id) {
-        require(challenges[_id].endTime > block.timestamp, "Challenge has ended");
+        Challenge memory challenge = challenges[_getChallengeIndex(_id)];
+        require(challenge.endTime > block.timestamp, "Challenge has ended");
         _;
     }
 
-    function clearUserContributions(uint256 _challengeId) external onlyOwner {
-        require(challenges[_challengeId].endTime <= block.timestamp, "Challenge has not ended yet");
-        delete _userContributions[_challengeId];
+    modifier onlyChallengeNotDisbursed(uint256 _id) {
+        Challenge memory challenge = challenges[_getChallengeIndex(_id)];
+        require(!challenge.isDisbursed, "Challenge has already been disbursed");
+        _;
+    }
+
+    function _getChallengeIndex(uint256 _challengeId) internal view returns (uint256) {
+        require(_challengeId > 0 && _challengeId <= challenges.length, "Invalid challenge id");
+        return _challengeId - 1;
     }
 
     function add(string memory _title, string memory _rules, uint256 duration) external payable {
         require(msg.value >= MIN_PRIZE_POOL, "You must send some ETH to the prize pool");
         require(duration > MIN_DURATION, "Challenge must be at least 1 day long");
+        require(duration < MAX_DURATION, "Challenge must be less than 15 days long");
 
         uint256 startTime = block.timestamp;
         uint256 endTime = startTime + duration;
-        challenges.push(Challenge(_title, _rules, 0, msg.value, startTime, endTime, msg.sender));
-        uint challengeId = challenges.length - 1;
+        challenges.push(Challenge(_title, _rules, false, 0, 0, startTime, endTime, msg.sender));
 
+        uint256 challengeId = _challengeIdCounter++;
         contribute(challengeId);
+        delete _userContributions[challengeId];
         emit ChallengeCreated(challengeId, _title, _rules, msg.value);
     }
 
     function contribute(uint256 _challengeId) public payable onlyChallengeNotEnded(_challengeId) {
         require(msg.value > 0, "Your contribution must be greater than 0");
         // Add contribution to user's balance for the challenge
-        uint256 totalContribution = _userContributions[_challengeId].get(msg.sender).add(msg.value);
-        _userContributions[_challengeId].set(msg.sender, totalContribution);
+        _userContributions[_challengeId].set(msg.sender, _userContributions[_challengeId].get(msg.sender) + msg.value);
         // add the contribution amount to the prize pool
-        challenges[_challengeId].prizePool += msg.value;
+        challenges[_getChallengeIndex(_challengeId)].prizePool += msg.value;
     }
 }
--- contracts/ChallengeHelper.sol
+++ contracts/ChallengeHelper.sol
@@ -3,47 +3,59 @@ pragma solidity ^0.8.19;
 
 import {EnumerableMapUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/structs/EnumerableMapUpgradeable.sol";
 import {ChallengeFactory} from "./ChallengeFactory.sol";
-import {FundDistributor} from "./FundDistributor.sol";
 
 contract ChallengeHelper is ChallengeFactory {
     using EnumerableMapUpgradeable for EnumerableMapUpgradeable.AddressToUintMap;
 
-    FundDistributor private _distributor;
+    uint256 public takeFee = 0.01 ether;
     mapping(address => uint256) internal _takerToChallenge;
     mapping(uint256 => mapping(address => mapping(address => bool))) internal _votedTakers;
-    mapping(uint256 => EnumerableMapUpgradeable.AddressToUintMap) internal _countedVotes;
+    mapping(uint256 => EnumerableMapUpgradeable.AddressToUintMap) internal _takerToVoteCount;
 
-    function initialize() public initializer {
-        __Ownable_init();
-        _distributor = new FundDistributor();
+    function setFee(uint256 _fee) external onlyOwner {
+        takeFee = _fee;
     }
 
-    function take(uint _challengeId) external onlyChallengeNotEnded(_challengeId) {
-        Challenge storage challenge = challenges[_challengeId];
+    function take(uint256 _challengeId) external payable onlyChallengeNotEnded(_challengeId)  {
+        require(msg.value == takeFee, "You must pay the take fee");
+
+        Challenge storage challenge = challenges[_challengeId - 1];
 
         require(msg.sender != challenge.creator, "You cannot take your own challenge");
+        require(_takerToChallenge[msg.sender] != _challengeId, "You can only take a challenge once");
 
         _takerToChallenge[msg.sender] = _challengeId;
         challenge.numTakers += 1;
     }
 
-    function vote(uint _challengeId, address _taker, bool _vote) external onlyChallengeNotEnded(_challengeId) {
+    function vote(uint256 _challengeId, address _taker, bool _vote) external onlyChallengeNotEnded(_challengeId) {
         require(_takerToChallenge[_taker] == _challengeId, "You can only vote for takers of this challenge");
 
-        require(
-            _userContributions[_challengeId].get(msg.sender) > 0,
-            "You must contribute to the challenge before voting"
-        );
+        require(_userContributions[_challengeId].get(msg.sender) > 0,"You must contribute to the challenge before voting");
 
-        require(
-            _votedTakers[_challengeId][msg.sender][address(0)] == false,
-            "You have already voted for this challenge"
-        );
+        require(!_votedTakers[_challengeId][msg.sender][_taker], "You can only vote once for each taker");
 
         _votedTakers[_challengeId][msg.sender][_taker] = _vote;
 
-        uint256 voteCount = _countedVotes[_challengeId].get(_taker);
-        _countedVotes[_challengeId].set(_taker, _vote ? voteCount + 1 : voteCount);
+        uint256 voteCount = _takerToVoteCount[_challengeId].get(_taker);
+        _takerToVoteCount[_challengeId].set(_taker, _vote ? voteCount + 1 : voteCount);
+    }
+
+    function getWinners(uint256 _challengeId) public view returns (address[] memory) {
+        uint256 maxVotes = 0;
+        address[] memory winners = new address[](0);
+
+        EnumerableMapUpgradeable.AddressToUintMap storage votes = _takerToVoteCount[_challengeId];
+        for (uint256 i = 0; i < votes.length(); i++) {
+            (address taker, uint256 voteCount) = votes.at(i);
+            if (voteCount > maxVotes) {
+                maxVotes = voteCount;
+                winners[0] = taker;
+            } else if (voteCount == maxVotes && voteCount > 0) {
+                winners[winners.length - 1] = taker;
+            }
+        }
+        return winners;
     }
 
     function distributePrizeOrRefund(uint _challengeId) external {
@@ -55,24 +67,43 @@ contract ChallengeHelper is ChallengeFactory {
 
         // if no one took the challenge, refund all contributors
         if (challenge.numTakers == 0) {
-            EnumerableMapUpgradeable.AddressToUintMap storage contributions = _userContributions[_challengeId];
-
-            for (uint i = 0; i < contributions.length(); i++) {
-                (address contributor, uint256 contribution) = contributions.at(i);
-                payable(contributor).transfer(contribution);
-            }
-
+            _refund(_challengeId);
             return;
         }
 
-        EnumerableMapUpgradeable.AddressToUintMap storage votes = _countedVotes[_challengeId];
-        // find the taker with the most votes
-        // address[] memory winners;
-        // uint256 maxVotes = 0;
-        // for (uint i = 0; i < votes.length(); i++) {
-        //     (address taker, uint256 voteCount) = votes.at(i);
+        address[] memory winners = getWinners(_challengeId);
+        // convert address[] to address payable[]
+        address payable[] memory winnersPayable = new address payable[](winners.length);
+        for (uint i = 0; i < winners.length; i++) {
+            winnersPayable[i] = payable(winners[i]);
+        }
+        _distributeFunds(challenge.prizePool, winnersPayable);
+    }
 
-        // }
-        // _distributor.distributeFunds(challenge.prizePool, challenge.numTakers);
+    function _refund(uint256 _challengeId) private {
+        EnumerableMapUpgradeable.AddressToUintMap storage contributions = _userContributions[_challengeId];
+        for (uint i = 0; i < contributions.length(); i++) {
+            (address contributor, uint256 contribution) = contributions.at(i);
+            payable(contributor).transfer(contribution);
+        }
+    }
+
+    function _distributeFunds(uint256 _prizePool, address payable[] memory _recipients) private onlyOwner {
+        require(_prizePool > 0, "Prize pool must be greater than 0");
+        require(_recipients.length > 0, "Must have at least one recipient");
+
+        uint256 numRecipients = _recipients.length;
+        uint256 amountPerRecipient = _prizePool / numRecipients;
+
+        // Ensure that the prize pool is evenly divisible among all recipients
+        require(_prizePool % numRecipients == 0, "Prize pool cannot be evenly distributed among recipients");
+
+        for (uint256 i = 0; i < numRecipients; i++) {
+            // Ensure that the recipient is not the zero address
+            require(_recipients[i] != address(0), "Recipient address cannot be zero");
+
+            // Transfer funds to the recipient
+            _recipients[i].transfer(amountPerRecipient);
+        }
     }
 }
