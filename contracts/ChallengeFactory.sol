b10f1b5c6264f3c88aa29f6dda352def5cb44f4a
datdoan92 <doantandat.fs@gmail.com>
2023-04-19 17:41:26 +0700

    fix: use Ownable and fix contribute logic

--- contracts/ChallengeFactory.sol
+++ contracts/ChallengeFactory.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
-import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import {EnumerableMapUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/structs/EnumerableMapUpgradeable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {EnumerableMap} from "@openzeppelin/contracts/utils/structs/EnumerableMap.sol";
 
-contract ChallengeFactory is OwnableUpgradeable {
-    using EnumerableMapUpgradeable for EnumerableMapUpgradeable.AddressToUintMap;
+contract ChallengeFactory is Ownable {
+    using EnumerableMap for EnumerableMap.AddressToUintMap;
 
     struct Challenge {
         string title;
@@ -18,12 +18,12 @@ contract ChallengeFactory is OwnableUpgradeable {
         address creator;
     }
 
-    mapping(uint256 => EnumerableMapUpgradeable.AddressToUintMap) internal _userContributions;
+    mapping(uint256 => EnumerableMap.AddressToUintMap) internal _userContributions;
 
-    uint256 public constant MIN_PRIZE_POOL = 0.01 ether;
-    uint256 public constant MIN_DURATION = 1 days;
-    uint256 public constant MAX_DURATION = 15 days;
     uint256 private _challengeIdCounter = 0;
+    uint256 public constant MIN_PRIZE_POOL = 0.01 ether;
+    uint32 public constant MIN_DURATION = 1 days;
+    uint32 public constant MAX_DURATION = 15 days;
 
     Challenge[] public challenges;
 
@@ -48,23 +48,25 @@ contract ChallengeFactory is OwnableUpgradeable {
 
     function add(string memory _title, string memory _rules, uint256 duration) external payable {
         require(msg.value >= MIN_PRIZE_POOL, "You must send some ETH to the prize pool");
-        require(duration > MIN_DURATION, "Challenge must be at least 1 day long");
-        require(duration < MAX_DURATION, "Challenge must be less than 15 days long");
+        require(duration > MIN_DURATION && duration < MAX_DURATION, "Challenge must be less than 15 days long");
 
         uint256 startTime = block.timestamp;
         uint256 endTime = startTime + duration;
         challenges.push(Challenge(_title, _rules, false, 0, 0, startTime, endTime, msg.sender));
 
-        uint256 challengeId = _challengeIdCounter++;
+        uint256 challengeId = ++_challengeIdCounter;
         contribute(challengeId);
-        delete _userContributions[challengeId];
         emit ChallengeCreated(challengeId, _title, _rules, msg.value);
     }
 
     function contribute(uint256 _challengeId) public payable onlyChallengeNotEnded(_challengeId) {
         require(msg.value > 0, "Your contribution must be greater than 0");
+        uint256 previousContribution = 0;
+        if (_userContributions[_challengeId].contains(msg.sender)) {
+            previousContribution = _userContributions[_challengeId].get(msg.sender);
+        }
         // Add contribution to user's balance for the challenge
-        _userContributions[_challengeId].set(msg.sender, _userContributions[_challengeId].get(msg.sender) + msg.value);
+        _userContributions[_challengeId].set(msg.sender, previousContribution + msg.value);
         // add the contribution amount to the prize pool
         challenges[_getChallengeIndex(_challengeId)].prizePool += msg.value;
     }
